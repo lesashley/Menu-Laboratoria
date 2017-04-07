2c10cefb0a010cfe5d929a903a46a4db51dfcd62
lesashley <lesashley56@gmail.com>
2017-04-06 22:22:30 -0500

    Funcion en js

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,14 @@ document.getElementById("toggle").addEventListener("click",function (e) {
   document.getElementById("nav-header").classList.toggle("open");
 
 })
+
+var lastScrollTop = 0;
+window.addEventListener("scroll", function () {
+  var currentScroll = window.pageYOffset || document.body.scrollTop;
+  if (currentScroll > lastScrollTop) {
+    console.log("down");
+  } else{
+    console.log("up");
+  }
+  lastScrollTop = currentScroll;
+})
